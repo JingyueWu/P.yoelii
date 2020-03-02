a9bf52ac2344cff1df983107e78caefe390d19a3
Jingyue Wu <61590108+JingyueWu@users.noreply.github.com>
2020-03-02 11:23:09 -0500

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,12 +1,38 @@
-# search the project nember, get the number of runs
-# create file runinfo.csv stores readable metadata
-esearch -db sra -query $1 | efetch -format runinfo > runinfo.csv
-# open runinfo.csv and cut the first column. Columns are separated by ","
-# locate lines starts with SRR, redirect them into runids.txt
+set -uex
+
+# Get the project number.
+PROJECT_ACC=$1
+
+# Directory to store fastq files.
+DATA=data
+
+# Directory to store fastqc files
+FASTQC=fastqc_out
+
+# Create fastqc_out directory
+mkdir -p $FASTQC
+
+# Directory to store multiqc output files
+MULTIQC=multiqc_out
+
+# Create multiqc output directory
+mkdir -p $MULTIQC
+
+# Get the number of runs and store metadata
+esearch -db sra -query $PROJECT_ACC | efetch -format runinfo > runinfo.csv
+
+
+# Get SRR ids from runinfo and store them in runids
 cat runinfo.csv | cut -f 1 -d ',' | grep SRR > runids.txt
-# run all SRR at once and only shows the first 10 paired ends
-cat runids.txt | parallel fastq-dump -X 10 --split-files {}
-# runs quality control on fastq files, produces .fastqc files
-fastqc --extract SRR2981459_1.fastq SRR2981460_1.fastq SRR2981461_1.fastq
-# run all fastqc files at once
-multiqc ./*fastqc*
+
+
+# Run all SRR at once and only shows the first 10 single ends
+cat runids.txt | parallel fastq-dump -X 10 {} -O $DATA
+
+
+# Run quality control on fastq files
+fastqc $DATA/*.fastq -o $FASTQC
+
+
+# Create a combined fastqc report of all runs
+multiqc $FASTQC -o $MULTIQC
